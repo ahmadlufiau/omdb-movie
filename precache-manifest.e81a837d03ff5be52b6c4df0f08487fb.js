50eb7312c10a1fa6db60ed5aeb7ff78d1609a1c3
Ahmad Lufi A U <ahmad@nextbase.co>
2019-12-18 13:58:34 +0700

    Updates

---
+++ precache-manifest.e81a837d03ff5be52b6c4df0f08487fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b9ac2640359a1805198886c49ec65e",
+    "url": "/omdb-movie/index.html"
+  },
+  {
+    "revision": "1081261162436dc10c5d",
+    "url": "/omdb-movie/static/css/main.7424e251.chunk.css"
+  },
+  {
+    "revision": "a0757d17b766fbd5d567",
+    "url": "/omdb-movie/static/js/2.643ae7bf.chunk.js"
+  },
+  {
+    "revision": "1081261162436dc10c5d",
+    "url": "/omdb-movie/static/js/main.fcb6ae0b.chunk.js"
+  },
+  {
+    "revision": "35ccea2548e05ba16f65",
+    "url": "/omdb-movie/static/js/runtime-main.06d23e60.js"
+  }
+]);
